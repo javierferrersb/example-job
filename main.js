07f753b22913d3a5b8faeb799176835bbebd37d5
Javier Ferrer Sánchez-Barcaiztegui <javierferrersb@gmail.com>
2024-01-29 21:14:12 +0100

    Expanded example

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-console.log("main.js");
-var foo = "bar";
-var bar = "foo";
+import { writeFileSync } from "fs";
+import { LoremIpsum } from "lorem-ipsum";
 
-console.log(foo);
-console.log(bar);
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4,
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4,
+  },
+});
+
+console.log(
+  "This is a test. The result is in the standard output.",
+  lorem.generateParagraphs(7)
+);
+
+writeFileSync(
+  "test.txt",
+  `Hello World. This is a test file testing the output in the file test.txt \n ${lorem.generateParagraphs(
+    10
+  )}`
+);
